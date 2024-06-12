8646b16a0ad549783ffafbb78eba78ea538d7f42
LispyJosh <jcroly1998@gmail.com>
2024-06-12 19:13:27 +0100

    snake starting, need to troubleshoot

--- index.js
+++ index.js
@@ -22,10 +22,45 @@ let foodX;  /*cordinates for our food */
 let foodY;
 let score = 0;
 
-/* making the snake */
+/* making the snake */ /* making an array of objects/body */
 
 let snake = [ /*Square brackets are used to define arrays. An array is an ordered collection of elements. */
+    {x:unitSize * 4 y:0},
+    {x:unitSize * 3 y:0},
+    {x:unitSize * 2, y:0}, /* seperate object due to comma */
+    {x:unitSize, y:0}, /*each one is a body part */
     {x:0, y:0} /*starting the snake at the top left */
-]
+];
 
-6:30
+/* adding an event listener to listen for key events */
+
+window.addEventListener("keydown", changeDirection); /* the even is keydown */
+resetBtn.addEventListener("click", resetGame) /* when we click resetbtn, we will envoke the reset game function */
+
+gameStart(); /*function for game */
+
+function gameStart(){};
+function nextTick(){};
+function clearBoard(){}; /*for re-painting the board*/
+function createFood(){
+    function randomFood(min, max){
+        const randNum = Math.round((Math.random() * (max - min) + min) / unitSize) * unitSize; /*this make it go in 25 size, idk*/
+        return randNum;
+    }
+    foodX = randomFood(0, gameWidth - unitSize); /*random number */
+    foodY = randomFood(0, gameWidth - unitSize);
+
+
+}; /* find a random place to place a food item*/
+function drawFood(){
+    ctx.fillStyle = foodColor;
+    ctx.fillRect(foodX, food, unitSize, unitSize); /*to fill a rectangle */
+}; /* I will have to paint the food within gameboard */
+function moveSnake(){};
+function drawSnake(){};
+function changeDirection(){};
+function checkGameOver(){};
+function displayGameOver(){};
+function resetGame(){};
+
+12:00- TROUBLE SHOOT
